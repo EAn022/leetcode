94e1b499d70b29f519dae0592ef7639454105c9e
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2025-07-13 17:31:04 -0300

    RichestCustomerWealth

--- AddTwoIntegers.java
+++ AddTwoIntegers.java
@@ -1,13 +1,11 @@
-package leetcode;
-
 public class AddTwoIntegers {
     public static void main(String[] args){
-        Solution somador = new Solution();
+        InnerAddTwoIntegers somador = new InnerAddTwoIntegers();
         System.out.println(somador.sum(10, 20));
     }
 }
 
-class Solution {
+class InnerAddTwoIntegers {
     public int sum (int num1, int num2){
         return num1 + num2;
     }
--- RunningSumof1dArray.java
+++ RunningSumof1dArray.java
@@ -1,14 +1,15 @@
 public class RunningSumof1dArray {
     public static void main(String[] args) {
-        
+        InnerRunningSumof1dArray solucao = new InnerRunningSumof1dArray();
+
     }
 }
 
-class Solution{
+class InnerRunningSumof1dArray{
     public int[] runningSum(int[] nums) {
         for(int i = 1; i < nums.length; i++){
             
-            nums[i] =nums[i] + nums[1-i];
+            nums[i] =nums[i] + nums[i-1];
         }
         return nums;
     }
