db6b92b1d3ef1c3093fb4ce5a377c3127eda9402
eduardoAN <eduardo.nunes16@fatec.sp.gov.br>
2025-07-19 10:30:48 -0300

    RansonNote2

--- RansomNote.java
+++ RansomNote.java
@@ -1,7 +1,9 @@
+import java.util.HashMap;
+
 public class RansomNote {
     public static void main(String[] args) {
-        InnerRansomNote solution = new InnerRansomNote();
-        System.out.println(solution.canConstruct("aa", "abab"));
+        InnerRansomNote2 solution = new InnerRansomNote2();
+        System.out.println(solution.canConstruct("aa", "bab"));
 
     }
 }
@@ -16,6 +18,29 @@ class InnerRansomNote {
             }
             int index = magazine.indexOf(ransomChar);
             magazine = magazine.substring(0, index) + magazine.substring(index + 1);
+        }
+        return true;
+    }
+}
+
+class InnerRansomNote2 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> magazineChars = new HashMap<>();
+
+        for(int i = 0; i < magazine.length(); i++){
+            char character = magazine.charAt(i);
+            
+            int currentCount = magazineChars.getOrDefault(character, 0);
+            magazineChars.put(character, currentCount + 1);
+        }
+
+        for(int i = 0; i < ransomNote.length(); i++){
+            char character = ransomNote.charAt(i);
+
+            int currentCount = magazineChars.getOrDefault(character, 0);
+            if(currentCount == 0) return false;
+
+            magazineChars.put(character, currentCount - 1);
         }
         return true;
     }
