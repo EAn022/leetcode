cb7cf07425a91cc60544c7b2e49bcae77303f5b1
eduardoAN <eduardo.nunes16@fatec.sp.gov.br>
2025-07-25 14:53:28 -0300

    MergeSortedArray

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -4,17 +4,24 @@ public class MergeSortedArray {
     public static void main(String[] a){
         int[] x = {2,3,0,0,0};
         int[] y = {1,2,7};
-        merge(x, x.length, y, y.length);
+        merge2(x, 2, y, 3);
     }
-    
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int nums1Pointer = 0, nums2Pointer = 0;
-        int[] aux = new int[m];
-        
-        for(int i = 0; i < m; i++){
-            if(Math.abs(nums1[nums1Pointer]) >= Math.abs(nums2[nums2Pointer])){
 
-            }
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int nums1Pointer = m - 1, nums2Pointer = n - 1, size = nums1.length - 1;
+        int[] aux = new int[size + 1];
+        
+         for(int i = size; i >= 0; i--){
+        if (nums1Pointer < 0) {
+            aux[i] = nums2[nums2Pointer--];
+        } else if (nums2Pointer < 0) {
+            aux[i] = nums1[nums1Pointer--];
+        } else if (nums1[nums1Pointer] > nums2[nums2Pointer]) {
+            aux[i] = nums1[nums1Pointer--];
+        } else {
+            aux[i] = nums2[nums2Pointer--];
         }
+    }
+        for (int i : aux) System.out.println(i);  
     }
 }
